5d3177e2ef34d841f81759a269cb2c4d2119c14b
Syuhadak27 <117827583+Syuhadak27@users.noreply.github.com>
2025-02-04 02:52:10 +0700

    Update config-sample.js

--- config-sample.js
+++ config-sample.js
@@ -2,6 +2,7 @@ export const config = {
   TOKEN: "", //token bot telegram
   WEBHOOK_URL: "",
   LOG_CHANNEL_ID: "",
+  CHANNEL_USERNAME: "",
   GOOGLE_API_KEY: "", //credential api key google sheet API
   SPREADSHEET_ID: ""  
 };
